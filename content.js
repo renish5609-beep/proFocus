c42c8f8746ce983d67f34d400c1556d31ded1796
renish5609-beep <renish5609@gmail.com>
2025-12-17 15:51:24 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,2 @@
-{
-  "manifest_version": 3,
-  "name": "proFocus",
-  "version": "0.1.0",
-  "description": "Remove distracting UI elements without blocking entire websites.",
-  "content_scripts": [
-    {
-      "matches": ["*://*.youtube.com/*"],
-      "js": ["content.js"]
-    }
-  ]
-}
+
 
